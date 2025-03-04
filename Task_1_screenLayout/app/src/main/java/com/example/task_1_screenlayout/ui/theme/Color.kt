34fce51926f7658a94077609029a5c51eeeeb062
Zafar Ibrogimov <zafar_ibrogimov@mail.ru>
2025-03-04 23:47:15 +0300

    completed the layout of the login screen

---
+++ Task_1_screenLayout/app/src/main/java/com/example/task_1_screenlayout/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.example.task_1_screenlayout.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+val colorDarkPurple = Color(0xFF7472A5)
+val colorLightOrange = Color(0xFFCC9880)
+val colorGreen = Color(0xFF8F937B)
+val textLightPurple = Color(0xFFAAA9C7)
+val colorBeige = Color(0xFFF2EEE3)
+
+val colorBackgroudWhite = Color(0xfff9f9f9)
+val colorFoundationGrey = Color(0xFF242424)
+val colorLightGrey = Color(0xFF9C9C9C)
+val colorLightRecGrey = Color(0xFFEDEDED)
+val colorDarkOrange = Color(0xFFC67C4E)
